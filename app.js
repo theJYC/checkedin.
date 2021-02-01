2fd0942fcb8625e1369994f3cf577a4c35ee2531
Jin Young Choi <jinyoungchoi@alumni.emory.edu>
2021-02-01 17:40:20 -0500

    preliminary design completed for landing page and modal box

--- app.js
+++ app.js
@@ -5,12 +5,10 @@ class Profile {
         firstName = "Unknown",
         lastInitial = "Unknown",
         checkInBy = "0",
-        priority = false;
     ) {
         this.firstName = firstName; // e.g. "Fredrick"
         this.lastInitial = lastInitial; // e.g. "T"
         this.checkInBy = checkInBy; // e.g. "01/22/21" (MM/DD/YY)
-        this.priority = priority; // e.g. true/false (Y/N)
     }
     onSchedule() {
         return pass; // this function will calculate the time since last contact
@@ -21,18 +19,18 @@ const fred = new Profile("Fredrick", "Thompson", "01/25/21", true);
 
 console.log(fred.firstName);
 
-console.log(fred.priority);
+console.log(fred.checkInBy);
 
 let myContacts = [];
 
 //When "add contact" is pressed:
-const addContactButton = document.getElementById("addContact");
+// const addContactButton = document.getElementById("contact");
 
-addContactButton.addEventListener("click", (e) => {
-    const target = e.target; // target attribute will return the specific button that is clicked
-    if (target.classList.contains("add-contact")) {
-        alert("you sure you want to add contact?")
-    }
-});
+// addContactButton.addEventListener("click", (e) => {
+//     const target = e.target; // target attribute will return the specific button that is clicked
+//     if (target.classList.contains("addContact")) {
+//         alert("you sure you want to add contact?")
+//     }
+// });
 
 
