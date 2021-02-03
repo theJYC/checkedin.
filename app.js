96ec7550f357574cd3a55da777da45ea5e1ce741
Jin Young Choi <jinyoungchoi@alumni.emory.edu>
2021-02-03 14:02:26 -0500

    created notes section across HTML,CSS,JS

--- app.js
+++ app.js
@@ -3,11 +3,13 @@
 class Profile {
     constructor(
         firstName = "Unknown",
-        lastInitial = "Unknown",
+        lastName = "Unknown",
+        notes = "Unknown",
         checkInBy = "0",
     ) {
         this.firstName = firstName; // e.g. "Fredrick"
-        this.lastInitial = lastInitial; // e.g. "T"
+        this.lastName = lastName; // e.g. "T"
+        this.notes = notes;
         this.checkInBy = checkInBy; // e.g. "01/22/21" (MM/DD/YY)
     }
 }
@@ -15,12 +17,15 @@ class Profile {
 //created an array to be populated upon user 'add contact' submission
 let contacts = [];
 
-const fred = new Profile("Fredrick", "Thompson", "01/25/21");
+// Sample Profile: Fredrick Thompson
+const fred = new Profile("Fredrick", "Thompson", "fulbright, taiwan", "06/07/21");
 
+//testing sample Profile:
+console.log("-----")
 console.log(fred.firstName);
-
+console.log(fred.lastName);
+console.log(fred.notes);
 console.log(fred.checkInBy);
-
 console.log("-----")
 
 
@@ -34,20 +39,19 @@ const toggleModal = () => {
 document.querySelector(".addContact").addEventListener("click", toggleModal);
 
 
-//preventing user from selecting past date on date input
-//found on StackOverFlow; solution does not work on mobile.
+//preventing user from selecting past date on date input (further info. in COMMENTS)
 const today = new Date().toISOString().split("T")[0];
 document.getElementsByName("setTodaysDate")[0].setAttribute("min", today);
 
 
-
-//fetching the input values from 'add contact' form:
+//grabbing input values from 'add contact' form:
 const getProfilefromInput = () => {
-    const firstInput = document.getElementById("first-name").value;
-    const lastInput = document.getElementById("last-name").value;
-    const dateInput = document.getElementById("check-in-by").value;
+    const firstName = document.getElementById("first-name").value;
+    const lastName = document.getElementById("last-name").value;
+    const notes = document.getElementById("notes-input").value;
+    const checkInBy = document.getElementById("check-in-by").value;
 
-    return new Profile(firstName, lastInitial, checkInBy)
+    return new Profile(firstName, lastName, notes, checkInBy);
 
 }
 
@@ -69,3 +73,15 @@ document.querySelector("#close")
 .addEventListener("click", toggleModal);
 
 //breaking down tasks into one actionable steps:
+
+
+/* COMMENTS
+
+-Preventing past date input function was derived from StackOverFlow
+and poses two immediate issues:
+    1) solution only works for calendar popup, but user can still manually type in past date in text form.
+        a) there will need to be an alert (etc.) function that prevents user from submitting the form entirely,
+           if past date is typed in.
+    2) allegedly the solution only works for Desktop, and not mobile.
+
+*/
