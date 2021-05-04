827238ea94a2a052fe65961c9a80e5c041452f0e
Jin Young Choi <jinyoungchoi@alumni.emory.edu>
2021-05-04 11:03:12 -0400

    debugging crUd restart js to latest change & html/css modified for updateFormBtn

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 //a one-liner snippet to incorpate dayjs library
 dayjs().format()
-
 //a function that resets placholder text for modal popup
 // (important feature with the update functionality in mind)
 const resetModal = () => {
@@ -11,7 +10,6 @@ const resetModal = () => {
     document.getElementById("check-in-by").value = "";
     document.getElementById("check-in-time").value = "09:00";
 }
-
 //a function that displays/hides modal popup
 const toggleModal = () => {
     document.querySelector(".modal") //selecting the modal element
@@ -32,57 +30,44 @@ document.querySelector(".addContact").addEventListener("click", () => {
     resetModal();
     toggleModal();
 });
-
 //another eventlistener for when "×" (close) button inside the modal is clicked
 document.querySelector("#close").addEventListener("click", () => {
     toggleModal();
     resetModal();
 });
-
 //a function that adds/removes display:hidden function of *prefilled* modal form
 //for the purpose of updating profile:
 const toggleUpdate = (item) => {
     document.querySelector(".modal") //selecting the modal element
     .classList.toggle("modal--hidden"); //call the classlist that hides the class
-
     document.getElementById("modaltitle").innerHTML = "edit check-in";
-
     //prefill the fields with the profile card values:
-
     console.log(item) // output is the specific Profile object
     console.log(item.randomId); // output is the randomId tied to the specific Profile object
-
     //first the names (stored in str format)
     document.getElementById("first-name").value = item.firstName;
     document.getElementById("last-name").value = item.lastInitial ? item.lastInitial[0] : "";
-
     //then, prefilling the notes text box with the notes (str format in localStorage)
     document.getElementById("notes-input").value = item.notes;
-
     //then, the date (stored as e.g. "2021-04-21", and time (e.g. "06:00"):
     document.getElementById("check-in-by").value = item.checkInBy;
     document.getElementById("check-in-time").value = item.checkInTime;
-
     //if update is submitted, reset the modal & delete the existing card (which can be found via randomId):
     document.querySelector("#submit").addEventListener("click", () => {
         resetModal();
-
         //logging the unique randomId for existing card for debugging
         console.log("this profilecard has the randomId of: ", item.randomId);
-
         //remove the og profilecard (with the referenced randomId) from the list!
         // 0): get index of original profilecard with above-logged randomId:
         const removeIndex = profilesList.map(item => item.randomId).indexOf(item.randomId)
         // 1): remove the profilecard
         profilesList.splice(removeIndex, 1);
-
         saveToLocalStorage();
         render();
     });
     //to make sure that once the [x] is clicked on updatemodal, the field values are reset.
     document.querySelector("#close").addEventListener("click", resetModal);
 };
-
 //Object constructor for profile to-be-submitted
 class Profile {
     constructor(firstName, lastInitial, notes, checkInBy, checkInTime, daysLeft, checkedIn, randomId) {
@@ -96,31 +81,23 @@ class Profile {
         this.randomId = randomId; // e.g. "6ab811e529". this is the unique identifier for each created profile (and reference when updated)
     }
 }
-
 //profilesList is an array that will at once be populated by user input
 //and later be saved to localStorage
 let profilesList = [];
-
 //day.js is to be used in lieu JS's native Date() object for more accuracy in date calulations
 //(see COMMENTS footnote)
-
 const calculateDueDate = date => {
-
     //retrieve today's date in "YYYY-MM-DD" format
     let dateToday = dayjs();
     //retrieve the user-input check-in date in "YYYY-MM-DD" format
     let datePicked = dayjs(date);
-
     //debugging purpose-- logging dayjs functionality to console
     console.log("dayjs is incorporated successfully");
-
     //diff. between current date & check-in date was calculated to its floating point value
     //instead of the integer value, since the former gives more accurate date calc.
     let inXDaysFloat = datePicked.diff(dateToday, "days", true);
-
     //for debugging purposes
     console.log(`${inXDaysFloat} days in float`)
-
     //e.g. 1.1234 days need to be converted to 2 day(s), not 1 day(s). therefore Math.ceil():
     let inXDays = Math.ceil(inXDaysFloat);
     console.log(inXDays);
@@ -140,18 +117,14 @@ const calculateDueDate = date => {
         return `in ${inXDays} days`;
     }
 }
-
 //initialising newProfile as an undefined variable on the global scope
 //to be used locally in addProfileToList:
 let newProfile;
-
 // added post-crUd functionality integration! ---
-
 //in order to best reference profile cards other than referencing via their indexes,
 // (which was discovered to be problematic when the update functionality is involved
 //  since the list will be dynamically changing; i.e. when the checkin profile is updated to a different date
 //  which will change the order (i.e. change its index in the profilesList array)
-
 //the function below generates a random hex string to give as id attribute to each profile card
 //so that each profile card can be referenced more precisely when performing crUd algorithm:
 const generateRandomId = () => {
@@ -163,23 +136,18 @@ const generateRandomId = () => {
     return output;
 }
 // -------
-
 //grabbing input values from 'add contact' form:
 const addProfileToList = () => {
     //consoling out to debug function
     console.log("submitting new profile...");
-
     //firstname to be stored as a variable to be manipulated for formatting below
     let firstNameStr = document.getElementById("first-name").value;
-
     //firstname input to make sure formatted output is e.g. 'Firstname'
     this.firstName = firstNameStr[0].toUpperCase() + firstNameStr.substring(1).toLowerCase();
     //last initial input to be formatted to uppercase e.g. 'L'
-
     //stored as variable to check whether user has included a lastname or not
     //which will determine whether formatting is necessary or not
     let lastInitialStr = document.getElementById("last-name").value;
-
     //format the lastInitial from "c" to "C." if user inputs a lastInitial value
     if (lastInitialStr) {
         this.lastInitial = `${document.getElementById("last-name").value.toUpperCase()}.`
@@ -187,67 +155,49 @@ const addProfileToList = () => {
     } else {
         this.lastInitial = '';
     }
-
     //notes input will have no additional formatting; just plain string data e.g. 'sample note'
     this.notes = document.getElementById("notes-input").value;
     //checkInBy will also be displayed as is inputted (via calendar widget or manual entry) e.g. 'YYYY-MM-DD'
     this.checkInBy = document.getElementById("check-in-by").value;
-
     this.checkInTime = document.getElementById("check-in-time").value;
-
     this.randomId = generateRandomId();
-
     //set checkedIn default value to false (i.e. "pending")
     //and staged for 'click' eventlistener that will convert it "complete"
     this.checkedIn = false;
     //adding custom variable to count days left till assigned check-in date:
     this.daysLeft = `${calculateDueDate(this.checkInBy)}`;
-
     //newProfile to be created and populated with the grabbed user input
     newProfile = new Profile(firstName, lastInitial, notes, checkInBy, checkInTime, daysLeft, checkedIn, randomId);
     console.log(newProfile);
-
     //newly constructed profile to populate the profilesList array defined above
     profilesList.push(newProfile);
-
     //log profilesList array with the user input pushed as last index.
     console.log(profilesList);
-
     saveToLocalStorage();
     console.log("saved to local storage");
     render();
     form.reset();
-
     //final console.log to make sure function is working from top to bottom
     console.log("profile submitted successfully")
 }
-
 // event listener to add profile to list when form is submitted
 const submitButton = document.getElementById("submit");
-
 submitButton.addEventListener("click", (event) => {
     //preventDefault prevents the form from its default activity; adding to the root url with form input queries.
     event.preventDefault();
     addProfileToList();
     toggleModal();
 });
-
-
 const saveToLocalStorage = () => {
     localStorage.setItem(`profilesList`, JSON.stringify(profilesList));
 }
-
 //preventing user from selecting past date on date input (further info. in COMMENTS)
 const today = new Date().toISOString().split("T")[0];
 document.getElementsByName("checkinby")[0].setAttribute("min", today);
-
-
 const render = () => {
     const cardContainer = document.getElementById("card-container");
     const profiles = document.querySelectorAll(".profile");
-
     profiles.forEach(profile => cardContainer.removeChild(profile));
-
     //sorting by checkInDate (does not incorporate checkInTime, as of 03/02/2021)
     profilesList.sort(function (a,b) {
         if (a.checkInBy < b.checkInBy) {
@@ -258,12 +208,10 @@ const render = () => {
         }
         return 0;
     })
-
     for (i = 0; i < profilesList.length; i++) {
         createProfile(profilesList[i]);
     }
 }
-
 //createProfile function enables user input to be displayed as profile cards.
 const createProfile = (item) => {
     const cardContainer = document.getElementById("card-container");
@@ -279,9 +227,7 @@ const createProfile = (item) => {
     const clearBtn = document.createElement("button");
     const checkedInWrapper = document.createElement("div");
     const checkedInBtn = document.createElement("button");
-
     //shaping the profile with .card & indexing each profile
-
     //if the contact is past-due on CheckedIn, paint the card container red!
     if ((dayjs(item.checkInBy).diff(dayjs(), "days", true)) <= -1) {
         profileDiv.classList.add("profile");
@@ -296,23 +242,18 @@ const createProfile = (item) => {
     nameDiv.textContent = `${item.firstName} ${item.lastInitial}`
     //indexing each contact for ease-of-reference
     profileDiv.setAttribute("id", profilesList.indexOf(item));
-
     profileDiv.appendChild(nameDiv)
-
     //then the dateText (a.k.a. "check-in by:" string)
     dateTextDiv.classList.add("datetext");
     dateTextDiv.textContent = "check in by:"
     profileDiv.appendChild(dateTextDiv);
-
     //next comes date
     dateDiv.classList.add("date");
     dateDiv.textContent = `${dayjs(item.checkInBy).format("MMM D")}`;
     profileDiv.appendChild(dateDiv);
-
     //next, the time of CheckIn
     timeDiv.classList.add("time");
     console.log(item.checkInTime);
-
     //firstly, checking that item.checkInTime input exists;
     //this is the debug solution to item.checkInTime.slice() method throwing an error on undefined
     //when no profile has been added to localstorage.
@@ -325,12 +266,10 @@ const createProfile = (item) => {
     //converting hour to int in order to perform int calculations for am/pm determination
     let checkInHourInt = parseInt(checkInHourStr);
     let checkInMinutes = item.checkInTime.slice(3); // grabbing minutes (00 to 59)
-
     let formattedTime = "";
     console.log(checkInHourInt); //str "00" to "12"
     console.log(typeof checkInHourInt)
     console.log(checkInMinutes);
-
     if (checkInHourInt == 0) {
         checkInHourInt = 12;
         formattedTime = `${checkInHourInt}:${checkInMinutes}am`;
@@ -342,16 +281,13 @@ const createProfile = (item) => {
         checkInHourInt -= 12; //format "23" to "11"
         formattedTime = `${checkInHourInt}:${checkInMinutes}pm`;
     }
-
     timeDiv.textContent = formattedTime;
     profileDiv.appendChild(timeDiv);
-
     //next, display text for daysLeft
     daysLeftDiv.classList.add("daysLeft");
     daysLeftDiv.textContent = calculateDueDate(item.checkInBy);
     console.log(daysLeftDiv.textContent);
     profileDiv.appendChild(daysLeftDiv);
-
     //next, add the notes display ('↠ notes ↞', with content available on mouseover)
     notesSpan.classList.add("notes");
     notesSpan.textContent = "↠note↞";
@@ -363,26 +299,21 @@ const createProfile = (item) => {
         notesSpan.setAttribute("aria-label", "n/a");
     }
     profileDiv.appendChild(notesSpan);
-
     //add button wrapper to encapsulate the two btns on top and have them aligned side-by-side
     btnWrapper.classList.add("btnwrapper");
     profileDiv.appendChild(btnWrapper);
-
     //next, format the updateBtn; this will be to the left of clearBtn
     updateBtn.classList.add("updatebtn");
     updateBtn.textContent = "update";
     btnWrapper.appendChild(updateBtn);
-
     //format the clearBtn
     clearBtn.classList.add("clearbtn");
     clearBtn.textContent = "clear";
     btnWrapper.appendChild(clearBtn);
-
     //checkedInBtn is going to be double width of other two btns,
     //so will be enclosed within a separate wrapper:
     checkedInWrapper.classList.add("checkedinwrapper");
     profileDiv.appendChild(checkedInWrapper);
-
     //then, format the checkedInBtn & append to its own wrapper
     checkedInBtn.classList.add("checkedinbtn");
     if (!item.checkedIn) {
@@ -393,27 +324,22 @@ const createProfile = (item) => {
         checkedInBtn.style.backgroundColor = "#607bbd";
     }
     checkedInWrapper.appendChild(checkedInBtn);
-
     //finally, adding the fully-done profileDiv to the list (#card-container)
     cardContainer.appendChild(profileDiv);
-
     //eventlistener for when update button is pressed
     updateBtn.addEventListener("click", () => {
         //first, toggle modal, but with all fields filled in with prior info.
-
         //arg (item) for toggleUpdate to be the specific profile object created via createProfile():
         toggleUpdate(item);
         saveToLocalStorage();
         render();
     })
-
     //eventlistener for when clear button is pressed
     clearBtn.addEventListener("click", () => {
         profilesList.splice(profilesList.indexOf(item),1);
         saveToLocalStorage();
         render();
     });
-
     //event listener for when checked in button is pressed
     checkedInBtn.addEventListener("click", () => {
         //when green pending btn is clicked, display the reverse state-- blue 'complete'
@@ -423,8 +349,6 @@ const createProfile = (item) => {
         render();
     });
 };
-
-
 const restore = () => {
     if(!localStorage.profilesList) {
         render();
@@ -437,5 +361,4 @@ const restore = () => {
         render();
     }
 }
-
 restore();
